1e0e6dac908f8446114b91793b36e498d71c85bc
Priyansh Gupta <priyansh.dev02@gmail.com>
2024-06-21 12:21:33 +0530

    Created the UI of IntroScreen

--- lib/screens/introscreen.dart
+++ lib/screens/introscreen.dart
@@ -11,7 +11,42 @@ class _IntroScreenState extends State<IntroScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+              Container(
+                height: MediaQuery.sizeOf(context).height*0.8,
+                width: MediaQuery.sizeOf(context).width*0.6,
+                child: Image.asset("assets/image/medailogo.png"),
+              ),
+            Container(
+                height: 50,
+                width: MediaQuery.sizeOf(context).width * 0.9,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        )),
+                    onPressed: () {
+                     /* Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Onboardingone()));*/
+                    },
+                    child: Text(
+                      'Get Started',
+                      style: TextStyle(
+                          fontFamily: 'SofiaPro',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    )),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
