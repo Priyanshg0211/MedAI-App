ea23a8e6dd54c7bcc531cc1ca59c331adc2f8cf6
Priyansh Gupta <priyansh.dev02@gmail.com>
2024-06-21 12:34:56 +0530

    created the UI of auth page

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -10,6 +11,78 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        automaticallyImplyLeading: false,
+        title: Center(
+          child: Text(
+            "MedAI",
+            style: TextStyle(
+              fontFamily: 'SofiaPro',
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            height: MediaQuery.sizeOf(context).height * 0.7,
+            width: MediaQuery.sizeOf(context).width,
+            child: LottieBuilder.asset("assets/image/medai intro.json"),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 50,
+            width: MediaQuery.sizeOf(context).width * 0.9,
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    )),
+                onPressed: () {
+                  /*  Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => AuthScreen()));*/
+                },
+                child: Text(
+                  'Login with Google',
+                  style: TextStyle(
+                      fontFamily: 'SofiaPro',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                )),
+          ),
+          Container(
+            height: 50,
+            width: MediaQuery.sizeOf(context).width * 0.9,
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    )),
+                onPressed: () {
+                  /*  Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => AuthScreen()));*/
+                },
+                child: Text(
+                  'Login with GitHub',
+                  style: TextStyle(
+                      fontFamily: 'SofiaPro',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                )),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/introscreen.dart
+++ lib/screens/introscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:med_ai/screens/auth_screen.dart';
 
 class IntroScreen extends StatefulWidget {
   const IntroScreen({super.key});
@@ -16,34 +17,32 @@ class _IntroScreenState extends State<IntroScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-              Container(
-                height: MediaQuery.sizeOf(context).height*0.8,
-                width: MediaQuery.sizeOf(context).width*0.6,
-                child: Image.asset("assets/image/medailogo.png"),
-              ),
             Container(
-                height: 50,
-                width: MediaQuery.sizeOf(context).width * 0.9,
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        )),
-                    onPressed: () {
-                     /* Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => Onboardingone()));*/
-                    },
-                    child: Text(
-                      'Get Started',
-                      style: TextStyle(
-                          fontFamily: 'SofiaPro',
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                    )),
-              ),
+              height: MediaQuery.sizeOf(context).height * 0.8,
+              width: MediaQuery.sizeOf(context).width * 0.6,
+              child: Image.asset("assets/image/medailogo.png"),
+            ),
+            Container(
+              height: 50,
+              width: MediaQuery.sizeOf(context).width * 0.9,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      )),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => AuthScreen()));
+                  },
+                  child: Text(
+                    'Get Started',
+                    style: TextStyle(
+                        fontFamily: 'SofiaPro',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  )),
+            ),
           ],
         ),
       ),
